da4d6fa201f09d2c2b3f242e93963dc55613614b
jyoti8824 <jyotisharma04120@gmail.com>
2024-08-22 19:21:01 +0530

    Update

---
+++ DrumKit/index.js
@@ -0,0 +1,8 @@
+document.querySelectorAll( "button.drum" ).forEach(
+    function ( b ) {
+        b.addEventListener( "click", function () {
+            alert( "I got clicked!" );
+        } );
+    } );
+
+
